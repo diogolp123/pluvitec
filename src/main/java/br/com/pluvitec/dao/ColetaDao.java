ed21dd1c4c5fe3522997c651256492b6448e294e
diogolp123 <diogo-lp@hotmail.com.br>
2019-07-02 01:02:23 -0300

    Refactory ColetaDao.java

--- src/main/java/br/com/pluvitec/dao/ColetaDao.java
+++ src/main/java/br/com/pluvitec/dao/ColetaDao.java
@@ -3,24 +3,32 @@ package br.com.pluvitec.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import br.com.pluvitec.dto.ColetaDto;
 import br.com.pluvitec.mapper.ColetaMapper;
+import br.com.pluvitec.model.Coleta;
 import br.com.pluvitec.repository.ColetaRepository;
 
-@Service
-public abstract class ColetaDao implements ColetaRepository {
+@Component
+public class ColetaDao {
+	
+	@Autowired
+	private ColetaRepository repository;
 	
 	@Autowired
 	private ColetaMapper mapper;
 	
 	public List<ColetaDto> findByMacAddress(String macAddress) {
-		return this.findByMacAddress(macAddress);
+		return repository.findByMacAddress(macAddress);
 	}
 	
 	public List<ColetaDto> findAllColetas() {
-		return mapper.toListColetaDto(this.findAll());
+		return mapper.toListColetaDto(repository.findAll());
+	}
+	
+	public void save(Coleta coleta) {
+		 repository.save(coleta);
 	}
 
 }
