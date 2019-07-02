68b4d13dd5b166058992c845e9e4e7edae145f7c
diogolp123 <diogo-lp@hotmail.com.br>
2019-07-02 00:48:01 -0300

    Bug fix

--- src/main/java/br/com/pluvitec/PluvitecApplication.java
+++ src/main/java/br/com/pluvitec/PluvitecApplication.java
@@ -2,12 +2,19 @@ package br.com.pluvitec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-public class PluvitecApplication {
+@SpringBootApplication(scanBasePackages="br.com.pluvitec")
+public class PluvitecApplication extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PluvitecApplication.class, args);
 	}
-
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
+	}
 }
--- src/main/java/br/com/pluvitec/dao/ColetaDao.java
+++ src/main/java/br/com/pluvitec/dao/ColetaDao.java
@@ -3,13 +3,13 @@ package br.com.pluvitec.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import br.com.pluvitec.dto.ColetaDto;
 import br.com.pluvitec.mapper.ColetaMapper;
 import br.com.pluvitec.repository.ColetaRepository;
 
-@Component
+@Service
 public abstract class ColetaDao implements ColetaRepository {
 	
 	@Autowired
